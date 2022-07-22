b7f209e0410677f898d1ef8537df7ddbe4d9430a
taos-ci <invain@naver.com>
2022-07-23 02:17:03 +0900

    2022-07-23 : Update nnstreamer.github.io
    Signed-off-by: taos-ci <invain@naver.com>

---
+++ assets/js/search/ssat-api.sh
@@ -0,0 +1,1 @@
+urls_downloaded_cb({"token":"ssat-api.sh","urls":[{"url":"how-to-write-testcase.html#case-1-gstlaunch-based-test-cases-bash-unit-test","node_type":"ul","context":{"gi-language":["default"]}}]});
